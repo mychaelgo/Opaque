de8982eca9e9199fffe4142924c2433cbdaeb78b
hendrikgouw <sacred_v_Cross@yahoo.com>
2014-01-17 11:11:18 +0700

    update sql
    added column(photo profile) and new table (table art for user artwork)

--- opaque.sql
+++ opaque.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.2
+-- version 3.2.4
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 06, 2014 at 11:47 AM
--- Server version: 5.5.32
--- PHP Version: 5.4.19
+-- Generation Time: Jan 17, 2014 at 11:03 AM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.1
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -23,6 +22,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `art`
+--
+
+CREATE TABLE IF NOT EXISTS `art` (
+  `artID` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` varchar(50) NOT NULL,
+  `artwork` varchar(100) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`artID`),
+  UNIQUE KEY `artID` (`artID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `art`
+--
+
+INSERT INTO `art` (`artID`, `userID`, `artwork`, `date`, `description`) VALUES
+(1, 'hendrik', '1.jpg', '2014-01-17 00:51:08', 'tescoba'),
+(2, 'hendrik', '2.jpg', '2014-01-17 00:51:08', 'bagus ga');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `favorite`
 --
 
@@ -33,6 +56,11 @@ CREATE TABLE IF NOT EXISTS `favorite` (
   `TargetType` varchar(50) NOT NULL COMMENT 'Belum tau ini apaan',
   PRIMARY KEY (`FavoriteId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `favorite`
+--
+
 
 -- --------------------------------------------------------
 
@@ -70,6 +98,11 @@ CREATE TABLE IF NOT EXISTS `mention` (
   PRIMARY KEY (`MentionId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Dumping data for table `mention`
+--
+
+
 -- --------------------------------------------------------
 
 --
@@ -82,6 +115,11 @@ CREATE TABLE IF NOT EXISTS `message` (
   `Date` datetime NOT NULL,
   PRIMARY KEY (`MessageId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `message`
+--
+
 
 -- --------------------------------------------------------
 
@@ -98,7 +136,7 @@ CREATE TABLE IF NOT EXISTS `tweet` (
   `TotalRetweet` int(11) NOT NULL,
   `TimeStamp` datetime NOT NULL,
   PRIMARY KEY (`TweetId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `tweet`
@@ -108,7 +146,8 @@ INSERT INTO `tweet` (`TweetId`, `UserId`, `Message`, `GeoCoordinates`, `IsRetwee
 (1, 'mychaelgo', 'mychaelgo 1', '', 'n', 1, '2013-11-08 00:00:00'),
 (2, 'mychaelgo', 'mychaelgo 2', '', 'n', 0, '2013-11-08 06:00:00'),
 (3, 'kwkw', 'testing dong', '', 'n', 0, '0000-00-00 00:00:00'),
-(4, 'hendrik', '123456', '', 'n', 0, '2014-01-06 09:37:31');
+(4, 'hendrik', '123456', '', 'n', 0, '2014-01-06 09:37:31'),
+(5, 'hendrik', 'tes aja nih boleh ga', '', '0', 0, '2014-01-16 22:40:40');
 
 -- --------------------------------------------------------
 
@@ -123,6 +162,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `TotalFollowing` int(11) NOT NULL,
   `TotalFollowers` int(11) NOT NULL,
   `Password` varchar(11) NOT NULL,
+  `Photo` varchar(100) NOT NULL,
   PRIMARY KEY (`UserId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -130,11 +170,11 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`UserId`, `FullName`, `AccountType`, `TotalFollowing`, `TotalFollowers`, `Password`) VALUES
-('haha', 'haha@haha', 0, 0, 0, 'haha'),
-('hendrik', 'Hendrik Prayogo', 1, 2, 2, '123'),
-('kwkw', 'kwkwkwkw', 0, 0, 0, 'kwkw'),
-('mychaelgo', 'Mychael Christian Go', 1, 1, 1, '123');
+INSERT INTO `user` (`UserId`, `FullName`, `AccountType`, `TotalFollowing`, `TotalFollowers`, `Password`, `Photo`) VALUES
+('haha', 'haha@haha', 0, 0, 0, 'haha', ''),
+('hendrik', 'Hendrik Prayogo', 1, 2, 2, '123', 'profile.jpg'),
+('kwkw', 'kwkwkwkw', 0, 0, 0, 'kwkw', ''),
+('mychaelgo', 'Mychael Christian Go', 1, 1, 1, '123', '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
